6025edbb6cc4e8bcf4260c61ff13fd02ce02ff7b
Ilya Drats <idrats@yandex.ru>
2020-05-31 19:26:08 +0300

    homework start

---
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/User.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.extentions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.unils.Utils
+import java.util.*
+
+fun User.toUserView(): UserView {
+    var nickname = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status =
+        if (lastVisit == null) "Ещё ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.humanizeDiff()}"
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        nickName = nickname,
+        initials = initials,
+        avatar = avatar,
+        status = status
+    )
+
+}
